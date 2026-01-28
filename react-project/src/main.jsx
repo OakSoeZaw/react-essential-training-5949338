8f37c843f516e486ff1ff515d48b91de7ceb830a
oaksoe <ozaw@oswego.edu>
2026-01-28 11:27:06 -0500

    leardt about arrays

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const [firstAnimal, second, third] = ["giraffe", "zebra", "bear"];
+console.log(firstAnimal)
+
 createRoot(document.getElementById("root")).render(<App />);
